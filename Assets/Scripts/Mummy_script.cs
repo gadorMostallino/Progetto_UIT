ebfdee1645266faa5cb17bf526a00a20f11970ec
gador <g.mostallino@studenti.unica.it>
2024-07-30 11:44:53 +0200

    Animazione sarcofago

--- Assets/Scripts/Mummy_script.cs
+++ Assets/Scripts/Mummy_script.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using MixedReality.Toolkit.SpatialManipulation;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Mummy_script : MonoBehaviour
 {
     private CubeInteractions_script cubeInteractionsScript;
     public GameObject sarcofago;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         cubeInteractionsScript = GameObject.Find("CuboMummia1").GetComponent<CubeInteractions_script>();
+        
     }
 
     // Update is called once per frame
@@ -43,12 +45,17 @@ public class Mummy_script : MonoBehaviour
         }
     }
 
-    void SetPositionMummy()
+    async void SetPositionMummy()
     {
         this.gameObject.transform.position = new Vector3((float)-1.71500003, (float)-0.233999997, (float)0.652999997);
         this.gameObject.transform.rotation = Quaternion.Euler((float)275.230896, (float)173.820313, (float)310.322205);
         this.gameObject.GetComponent<ObjectManipulator>().enabled = false;
         GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         gameManager.NextPanel(10);
+        
+        await Task.Delay(500);
+        GameObject topSarcophagus = GameObject.Find("top"); 
+        topSarcophagus.GetComponent<Animator>().Play("ClosingSarcophagus");
+        topSarcophagus.GetComponent<AudioSource>().Play();
     }
 }
