47b8cd3da86dadbe3c8d3ca681c178bbf1773fe1
elisaarca <e.arca3@studenti.unica.it>
2024-08-17 00:36:41 +0200

    Aggiunto rumore bendaggio

--- Assets/Scripts/Bendage_script.cs
+++ Assets/Scripts/Bendage_script.cs
@@ -27,28 +27,32 @@ public class Bendage_script : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Cube1"))
         {
+            cube1.GetComponent<AudioSource>().Play();
             //cube change color to red when collision happens
             cube1.GetComponent<Renderer>().material = red;
             cube2.SetActive(true);
-
         }
         if (other.gameObject.CompareTag("Cube2"))
         {
+            cube2.GetComponent<AudioSource>().Play();
             cube2.GetComponent<Renderer>().material = red;
             cube3.SetActive(true);
         }
         if (other.gameObject.CompareTag("Cube3"))
-        {
+        {   
+            cube3.GetComponent<AudioSource>().Play();
             cube3.GetComponent<Renderer>().material = red;
             cube4.SetActive(true);
         }
         if (other.gameObject.CompareTag("Cube4"))
         {
+            cube4.GetComponent<AudioSource>().Play();
             cube4.GetComponent<Renderer>().material = red;
             cube5.SetActive(true);
         }
         if (other.gameObject.CompareTag("Cube5"))
         {
+            cube5.GetComponent<AudioSource>().Play();
             GameObject.Find("Step4Cubes").SetActive(false);
             GameObject.Find("Man").SetActive(false);
             mummy.SetActive(true);
